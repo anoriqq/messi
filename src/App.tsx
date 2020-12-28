19153a4d03923f5f28b02fd192fd903f01f29ecd
anoriqq <shota.yoshikawa@anoriqq.com>
2020-12-28 14:13:08 +0900

    feat: messi

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
+import InputFile from './components/inputFile'
+import MemberList, {Members} from './components/memberList'
+import CreateTeam from './components/createTeam'
 
 function App() {
+  const [members, setMembers] = useState<Members>([])
   return (
     <div className="App">
-      <b>sample</b>
+      <InputFile
+        type='file'
+        onChange={e => onFileChange(e, setMembers)}
+      />
+      <MemberList
+        members={members}
+      />
+      <CreateTeam
+        members={members}
+      />
     </div>
   );
+}
+
+const onFileChange= async (e: ChangeEvent<HTMLInputElement>, setMembers: React.Dispatch<React.SetStateAction<Members>>) => {
+  let text = await e.target.files?.item(0)?.text()
+  if (!text) return
+  const json: {members: Members} = JSON.parse(text)
+  setMembers(json.members)
 }
 
 export default App;
